8d7488b44b7d2850c5d33f56294ad5e846ee4222
godddesseyes <goddesseyes@protonmail.com>
2020-08-21 18:46:17 +1000

    Add build for heroku

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.773"]
